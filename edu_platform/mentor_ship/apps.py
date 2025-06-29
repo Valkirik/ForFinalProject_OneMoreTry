8c7434e336bf16813829e1360270eeb1cd84b76d
Valery <valkirik1995@mail.ru>
2025-06-29 20:38:51 +0300

    everything ready for runserver (but without docker)

--- edu_platform/mentor_ship/apps.py
+++ edu_platform/mentor_ship/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MentorShipConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mentor_ship'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mentor_ship"
