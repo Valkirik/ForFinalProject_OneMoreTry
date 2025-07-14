30998070ca0848e5ff8f52a7cc8eae48f3a104a7
Valery <valkirik1995@mail.ru>
2025-07-14 21:43:29 +0300

    create models: Test, Questuions, Answer

--- edu_platform/testing_sistem/admin.py
+++ edu_platform/testing_sistem/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Answer, Article, Course, Questions, Test, Topic
 
 admin.site.register(Course)
+admin.site.register(Topic)
+admin.site.register(Article)
+admin.site.register(Test)
+admin.site.register(Questions)
+admin.site.register(Answer)
