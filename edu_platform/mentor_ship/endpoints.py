e9e6b40bea3f79f66242b9a550dd2e13de1390db
Valery <valkirik1995@mail.ru>
2025-07-21 03:23:17 +0300

    somecorrections with the endpoint

--- edu_platform/mentor_ship/endpoints.py
+++ edu_platform/mentor_ship/endpoints.py
@@ -1,29 +1,37 @@
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework.generics import ListAPIView
-from .models import User, Specialisation, Student, Teacher, Group
-from .serializer import UserSerializer, SpecialisationSerializer, StudentSerializer, TeacherSerializer, GroupSerializer, GroupStudentSerializer
 from itertools import chain
+
+from rest_framework import permissions, viewsets
+from rest_framework.generics import ListAPIView
+
+from .models import Group, Specialisation, Student, Teacher, User
+from .serializer import (GroupSerializer, GroupStudentSerializer,
+                         SpecialisationSerializer, StudentSerializer,
+                         TeacherSerializer, UserSerializer)
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+
 class SpecialisationViewSet(viewsets.ModelViewSet):
     queryset = Specialisation.objects.all()
     serializer_class = SpecialisationSerializer
     permissions = [permissions.AllowAny]
+
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     permissions = [permissions.AllowAny]
 
+
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
     permissions = [permissions.AllowAny]
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
@@ -31,11 +39,13 @@ class GroupViewSet(viewsets.ModelViewSet):
     permissions = [permissions.AllowAny]
 
 
-#here we get the list with the group and the students of these group
+# here we get the list with the group and the students of these group
+
 
 class AllGroupStudentListAPIview(ListAPIView):
     serializer_class = GroupStudentSerializer
     permissions = [permissions.AllowAny]
+
     def get_queryset(self):
         group_queryset = Group.objects.all()
         student_queryset = Student.objects.all()
@@ -43,13 +53,14 @@ class AllGroupStudentListAPIview(ListAPIView):
         return queryset
 
 
-#here we get all the students from the certain group
+# here we get all the students from the certain group
+
 
 class StudentsGroupListAPIview(ListAPIView):
     serializer_class = StudentSerializer
-    permissions = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny]
+
     def get_queryset(self):
         group_id = self.kwargs["id"]
-        student_queryset = Student.objects.all()
-        return student_queryset
-
+        student = Student.objects.filter(group__in=group_id)
+        return student
--- edu_platform/mentor_ship/urls.py
+++ edu_platform/mentor_ship/urls.py
@@ -1,19 +1,19 @@
 from django.urls import include, path, re_path
 from rest_framework import routers
-from rest_framework.routers import SimpleRouter
-from .endpoints import UserViewSet, SpecialisationViewSet, StudentViewSet, TeacherViewSet, \
-    GroupViewSet, AllGroupStudentListAPIview, StudentsGroupListAPIview
+
+from .endpoints import (AllGroupStudentListAPIview, GroupViewSet,
+                        SpecialisationViewSet, StudentsGroupListAPIview,
+                        StudentViewSet, TeacherViewSet, UserViewSet)
 
 router = routers.SimpleRouter()
-router.register('user_viewset', UserViewSet)
-router.register('specialisation_viewset', SpecialisationViewSet)
-router.register('student_viewset', StudentViewSet)
-router.register('teacher_viewset', TeacherViewSet)
-router.register('group_viewset', GroupViewSet)
+router.register("user_viewset", UserViewSet)
+router.register("specialisation_viewset", SpecialisationViewSet)
+router.register("student_viewset", StudentViewSet)
+router.register("teacher_viewset", TeacherViewSet)
+router.register("group_viewset", GroupViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
     path("group_student/", AllGroupStudentListAPIview.as_view()),
     re_path("group/(?P<id>.+)/student_list", StudentsGroupListAPIview.as_view()),
-
 ]
